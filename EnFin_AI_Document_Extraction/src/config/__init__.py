fcb358298b055bdb7b8a6ecbfd7ddbcd985152f2
govindnox <89859301+govindnox@users.noreply.github.com>
2026-04-23 13:04:59 +0530

    Delete EnFin_AI_Document_Extraction directory

--- EnFin_AI_Document_Extraction/src/config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Configuration package for document extraction Lambda.
-
-Author: Reet Roy
-Version: 1.1.0
-"""
-
-from config.settings import config  # noqa: F401
--- EnFin_AI_Document_Extraction/src/scoring/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Scoring package for document extraction Lambda.
-
-Author: Reet Roy
-Version: 1.0.0
-"""
